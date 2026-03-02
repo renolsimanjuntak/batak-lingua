a72800178842c374ad23d19cc8bdb52bd3d3d04c
Renol Simanjuntak <renol@outlook.it>
2026-03-02 21:21:24 +0700

    add toba-map.js

---
+++ data/toba-map.js
@@ -0,0 +1,45 @@
+/**
+ * @project   Batak Lingua
+ * @file      toba-map.js
+ * @author    Renol Simanjuntak
+ * @link      https://github.com/renolsimanjuntak/batak-lingua
+ * @license   MIT License - Copyright (c) 2026 Batak Lingua
+ */
+
+const BATAK_TOBA_MAP = {
+    "ina_ni_surat": {
+        "a":   "\u1BC0",
+        "ha":  "\u1BC2",
+        "ka":  "\u1BC2",
+        "ma":  "\u1BD4",
+        "na":  "\u1BC9",
+        "ra":  "\u1BD2",
+        "ta":  "\u1BD6",
+        "ba":  "\u1BC5",
+        "pa":  "\u1BC7",
+        "la":  "\u1BDE",
+        "ga":  "\u1BCE",
+        "ja":  "\u1BD0",
+        "da":  "\u1BD1",
+        "nga": "\u1BDD",
+        "wa":  "\u1BCB",
+        "ya":  "\u1BDB",
+        "sa":  "\u1BD8",
+        "ca":  "\u1BE1",
+        "nya": "\u1BE0",
+        "i":   "\u1BE4",
+        "u":   "\u1BE5"
+    },
+    "anak_ni_surat": {
+        "i":   "\u1BEA",
+        "u":   "\u1BEE",
+        "e":   "\u1BE9",
+        "o":   "\u1BEC",
+        "ng":  "\u1BF0"
+    },
+    "special": {
+        "pangolat": "\u1BF2"
+    }
+};
+
+export default BATAK_TOBA_MAP;
